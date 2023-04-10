8e4424e658147e4e0fbc4d98dcb3dbb6959e8e79
mak453 <mak453@pitt.edu>
2023-04-10 01:26:22 -0400

    Nothing works ughhhhhhhh

---
+++ Class_Definitions/bode_cap.m
@@ -0,0 +1,9 @@
+function b = bode_cap(fig, cap)  %input capacitor object, returns bode plot
+    C = cap.capacitance;    %get capacitance
+    sys = tf([1], [C 0]);  %create transfer function (reactance)
+    p = uipanel(fig);
+    p.Layout.Row = [5 8]
+    p.Layout.Column = [5 8]
+    b = bodeplot(sys);      %get bode plot (frequency vs reactance)
+    
+end
--- Class_Definitions/capacitor.m
+++ Class_Definitions/capacitor.m
@@ -1,20 +1,21 @@
+<<<<<<< Updated upstream
 classdef capacitor
     properties (Constant, Access = private)
+=======
+classdef capacitor < handle
+    properties (Constant, Access = protected)
+>>>>>>> Stashed changes
       e0 = 8.854e-12
       valid = false
     end
-    
-    properties (Access = private)
-        dielectric
-    end
-    
+      
     properties
         area
         capacitance
         max_voltage
         max_charge
-        plate_distance
-        material
+        A_dist
+        dielectric
         reactance
     end
     
@@ -28,9 +29,9 @@ classdef capacitor
             end
                 
             obj.dielectric = material;
-            obj.material = material.name;
             obj.capacitance = cap;
             obj.area = area;
+<<<<<<< Updated upstream
             obj.plate_distance = dist;
             
             obj.solve(obj)
@@ -46,6 +47,23 @@ classdef capacitor
                 obj.area = (obj.capacitance*obj.plate_distance)/(e0*obj.dielectric.er);
             elseif obj.capacitance > 0 && obj.area > 0 && obj.plate_distance == 0
                 obj.plate_distance = (e0*obj.dielectric.er*obj.area)/obj.capacitance;
+=======
+            obj.A_dist = dist;
+            disp(obj)
+            obj.solve(obj);
+        end
+    end
+    
+    methods (Static)
+        function obj = solve(obj) 
+            % C = (e0*er*A)/d            
+            if obj.capacitance == 0 && obj.area > 0 && obj.A_dist > 0
+                obj.capacitance = (obj.dielectric.er*obj.e0* obj.area)/obj.A_dist; 
+            elseif obj.capacitance > 0 && obj.area == 0 && obj.A_dist > 0
+                obj.area = (obj.capacitance*obj.A_dist)/(obj.e0*obj.dielectric.er);
+            elseif obj.capacitance > 0 && obj.area > 0 && obj.A_dist == 0
+                obj.A_dist = (obj.e0*obj.dielectric.er*obj.area)/obj.capacitance;
+>>>>>>> Stashed changes
             else
                 % error
             end
